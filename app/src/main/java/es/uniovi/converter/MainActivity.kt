e67deb7a860f3ab05e1be4a6d5fd73a2cc9a06f2
uo294319 <uo294319@uniovi.es>
2025-12-09 15:09:16 +0100

    3.7 Converter Simple

--- app/src/main/java/es/uniovi/converter/MainActivity.kt
+++ app/src/main/java/es/uniovi/converter/MainActivity.kt
@@ -2,6 +2,7 @@ package es.uniovi.converter
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val euroToDollar:Double = 1.16;
+    lateinit var editTextEuros: EditText;
+    lateinit var editTextDollars:EditText;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,13 +23,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        editTextEuros = findViewById<EditText>(R.id.editTextEuros)
+        editTextDollars = findViewById<EditText>(R.id.editTextDollars)
     }
 
     fun onClickToDollars(view: View) {
-        Toast.makeText(this, "¡Me han pulsado!", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "¡Me han pulsado!", Toast.LENGTH_SHORT).show()
+        convert(editTextEuros, editTextDollars, euroToDollar)
     }
 
     fun onClickToEuros(view: View) {
-        Toast.makeText(this, "¡Me han pulsado!", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "¡Me han pulsado!", Toast.LENGTH_SHORT).show()
+        convert(editTextDollars, editTextEuros, 1 / euroToDollar)
+    }
+
+    private fun convert(source: EditText, destination: EditText, factor: Double) {
+        val text = source.text.toString()
+        val value = text.toDoubleOrNull()
+        if (value == null) {
+            destination.setText("")
+            return
+        }
+        destination.setText((value * factor).toString())
     }
 }
